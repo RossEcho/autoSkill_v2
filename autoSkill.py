26da99280ae36166799dc222c39e7482f390ca08
RossEcho <ros303ros@gmail.com>
2023-07-26 20:58:05 +0300

    Update autoSkill.py
    added hp bar location set option to resolve the resolution issue.

--- autoSkill.py
+++ autoSkill.py
@@ -22,24 +22,25 @@ SOFTWARE.
 """
 
 import tkinter as tk
-from tkinter import messagebox, Scale, HORIZONTAL
+from tkinter import messagebox
 import cv2
 import numpy as np
 import pyautogui
 import threading
 import time
 import keyboard
+from PIL import Image
 
 class AutoKeyPresser(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Auto Key Presser")
-        self.geometry("300x600")
+        self.geometry("350x650")
 
         self.key_to_press = [tk.StringVar(self) for _ in range(5)]
         self.frequency = [tk.DoubleVar(self) for _ in range(5)]
         self.hp_key = tk.StringVar(self)
-        self.hp_level = tk.DoubleVar(self, value=80)  # Initial threshold set to 80%
+        self.hp_level = tk.DoubleVar(self, value=80)
         self.threads = []
         self.entries = []
 
@@ -47,23 +48,25 @@ class AutoKeyPresser(tk.Tk):
             tk.Label(self, text=f"Key to Press {i+1}:").pack()
             entry = tk.Entry(self, textvariable=self.key_to_press[i])
             entry.pack()
-            self.entries.append(entry)
 
             tk.Label(self, text=f"Frequency {i+1} (seconds):").pack()
             entry = tk.Entry(self, textvariable=self.frequency[i])
             entry.pack()
-            self.entries.append(entry)
 
         tk.Label(self, text="HP Key:").pack()
         entry = tk.Entry(self, textvariable=self.hp_key)
         entry.pack()
-        self.entries.append(entry)
 
         tk.Label(self, text="HP Bar Image Similarity:").pack()
-        self.hp_scale = Scale(self, from_=0, to=100, length=200, orient=HORIZONTAL,  # Set scale range from 0 to 100
-                              variable=self.hp_level, resolution=1, sliderlength=30,
-                              troughcolor='gray', sliderrelief='raised')
+        self.hp_scale = tk.Scale(self, from_=0, to=100, length=200, orient=tk.HORIZONTAL,
+                                 variable=self.hp_level, resolution=1, sliderlength=30,
+                                 troughcolor='gray', sliderrelief='raised')
         self.hp_scale.pack()
+
+        # HP bar setup instructions
+        tk.Label(self, text="To set up the HP bar region, go to your game window and:\n"
+                            "Press F1 at the start of the HP bar.\n"
+                            "Press F2 at the end of the HP bar.").pack()
 
         self.monitor_hp = tk.BooleanVar(self)
         self.hp_checkbox = tk.Checkbutton(self, text="Monitor HP", variable=self.monitor_hp)
@@ -74,18 +77,19 @@ class AutoKeyPresser(tk.Tk):
 
         self.should_press = threading.Event()
 
-        # Register the hotkey
-        keyboard.add_hotkey('F1', self.toggle_script)
+        keyboard.add_hotkey('F1', self.set_hp_bar_start)
+        keyboard.add_hotkey('F2', self.set_hp_bar_end)
+        keyboard.add_hotkey('F3', self.toggle_script)
 
         self.full_bar_reference = None
-        self.gray_bar_reference = cv2.imread("gray_hp.png")  # Load the image of the empty HP bar
-        self.threshold = None
+        self.gray_bar_reference = cv2.imread("gray_hp.png")
+        self.reference_height, self.reference_width = self.gray_bar_reference.shape[:2]
 
-        # Set your HP bar area
-        self.hp_bar_x1 = 585
-        self.hp_bar_y1 = 930
-        self.hp_bar_x2 = 630
-        self.hp_bar_y2 = 1045
+        self.hp_bar_x1 = None
+        self.hp_bar_y1 = None
+        self.hp_bar_x2 = None
+        self.hp_bar_y2 = None
+
 
     def toggle_script(self):
         if self.should_press.is_set():
@@ -99,13 +103,10 @@ class AutoKeyPresser(tk.Tk):
         self.hp_checkbox.config(state='disabled')
         self.start_pressing()
 
-        # Create the thread but don't start it yet
         hp_monitor_thread = threading.Thread(target=self.monitor_hp_and_press_key)
 
         if self.monitor_hp.get():
-            # Run the scan in the current thread, so we will wait for it to finish
             self.scan_full_hp_bar(20)
-            # Now we can start the HP monitor thread
             hp_monitor_thread.start()
 
         messagebox.showinfo("Started", "Key pressing has started.")
@@ -126,11 +127,11 @@ class AutoKeyPresser(tk.Tk):
         while self.should_press.is_set():
             hp_percentage = self.get_hp_percentage()
             scaled_hp_percentage = self.scale_percentage(hp_percentage)
-            print(f'Current HP similarity: {scaled_hp_percentage}%')  # Print the scaled HP similarity
+            print(f'Current HP similarity: {scaled_hp_percentage}%')
             if scaled_hp_percentage is not None and scaled_hp_percentage < self.hp_level.get():
                 pyautogui.press(self.hp_key.get())
-                time.sleep(1)  # Delay before next HP check
-            time.sleep(0.1)  # Adjust this value to control how often the HP check is performed when not healing
+                time.sleep(1)
+            time.sleep(0.1)
 
     def stop(self):
         threading.Thread(target=self.stop_pressing).start()
@@ -149,6 +150,23 @@ class AutoKeyPresser(tk.Tk):
         threading.Thread(target=self.stop_pressing).start()
         self.destroy()
 
+    def set_hp_bar_start(self):
+        x, y = pyautogui.position()
+        self.hp_bar_x1 = x
+        self.hp_bar_y1 = y
+        messagebox.showinfo("Info", f"Start point of HP bar set to: {x}, {y}")
+
+    def set_hp_bar_end(self):
+        x, y = pyautogui.position()
+        if x < self.hp_bar_x1:
+            self.hp_bar_x1, x = x, self.hp_bar_x1
+        if y < self.hp_bar_y1:
+            self.hp_bar_y1, y = y, self.hp_bar_y1
+        self.hp_bar_x2 = x
+        self.hp_bar_y2 = y
+        messagebox.showinfo("Info", f"End point of HP bar set to: {x}, {y}")
+
+
     def scan_full_hp_bar(self, duration):
         print("Scanning for full HP bar color range...")
         end_time = time.time() + duration
@@ -165,13 +183,15 @@ class AutoKeyPresser(tk.Tk):
         hp_bar_pixels = self.capture_hp_bar()
         result = cv2.matchTemplate(self.gray_bar_reference, hp_bar_pixels, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, _ = cv2.minMaxLoc(result)
-        return max_val * 100  # similarity percentage
+        return max_val * 100
 
     def capture_hp_bar(self):
         screenshot = pyautogui.screenshot()
         hp_bar_region = screenshot.crop((self.hp_bar_x1, self.hp_bar_y1, self.hp_bar_x2, self.hp_bar_y2))
-        return np.array(hp_bar_region)
-        
+        hp_bar_region_resized = hp_bar_region.resize((self.reference_width, self.reference_height), 3)  # 3 corresponds to ANTIALIAS
+        return np.array(hp_bar_region_resized)
+
+
     def scale_percentage(self, value):
         old_min, old_max = -20, 27
         new_min, new_max = 0, 100
@@ -180,7 +200,6 @@ class AutoKeyPresser(tk.Tk):
         scaled_value = (((value - old_min) * new_range) / old_range) + new_min
         return scaled_value
 
-
     def run(self):
         self.protocol("WM_DELETE_WINDOW", self.close_window)
         super().mainloop()
@@ -188,24 +207,3 @@ class AutoKeyPresser(tk.Tk):
 if __name__ == "__main__":
     app = AutoKeyPresser()
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
